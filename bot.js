c90a7081fc933f0bd66e64b1bae6a47c0f4d31b6
mehdisebbahi <42147862+mehdisebbahi@users.noreply.github.com>
2018-08-10 14:09:09 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+سلام عليكم
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/teKnWcX ]
+الدعووة خاصة لك ... [ MedoxYT  ]
+**`)
+}).catch(console.error)
+})
+A7MD.login('NDc1NzcxMDcxNjM1NjUyNjE5.DknEww.zKaF0d9wCsLne8JHDgn3W96N5LQ');
